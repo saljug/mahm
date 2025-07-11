2b95cfb572bc66f486e41e00698be7f51ea368c5
Saljug Mahmudlu <saljugmahmudlu@gmail.com>
2025-07-11 19:43:13 +0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { inject } from '@vercel/analytics'
 
+inject()
 createRoot(document.getElementById("root")!).render(<App />);
